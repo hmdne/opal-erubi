b238a6515566ee185afa35a4458ae1aa2cfeacc9
hmdne <54514036+hmdne@users.noreply.github.com>
2020-08-02 11:58:24 +0200

    Support Sprockets 4.0! ref: opal/opal-sprockets#19

--- Gemfile
+++ Gemfile
@@ -3,6 +3,16 @@ source 'https://rubygems.org'
 gemspec
 
 gem "opal-rspec", "> 0.6.2"
-gem "opal"
+
+if File.exist? "../opal"
+  gem "opal", path: "../opal"
+else
+  gem "opal"
+end
+
+if File.exist? "../opal-sprockets"
+  gem "opal-sprockets", path: "../opal-sprockets"
+end
+
 gem "erubi"
 
--- lib/opal/erubi/processor.rb
+++ lib/opal/erubi/processor.rb
@@ -8,7 +8,7 @@ module Opal
       self.default_mime_type = 'application/javascript'
 
       def evaluate(context, locals, &block)
-        @data = Opal::Erubi.compile_erubi @data, context.logical_path.sub(/^templates\//, '')
+        @data = Opal::Erubi.compile_erubi @data, context.logical_path.gsub(/\Atemplates\//, '')
         super
       end
 
@@ -20,10 +20,15 @@ module Opal
   end
 end
 
-if Sprockets.respond_to? :register_transformer
-  extra_args = [{mime_type: 'application/javascript', silence_deprecation: true}]
+if Opal::Sprockets.respond_to? :register_mime_type # Sprockets 4.0 support
+  Sprockets.register_mime_type 'application/html+erubi', extensions: ['.erubi']
+  Sprockets.register_transformer 'application/html+erubi', 'application/javascript', Opal::Erubi::Processor
+  Opal::Sprockets.register_mime_type 'application/html+erubi'
 else
-  extra_args = []
+  if Sprockets.respond_to? :register_transformer
+    extra_args = [{mime_type: 'application/javascript', silence_deprecation: true}]
+  else
+    extra_args = []
+  end
+  Sprockets.register_engine '.erubi', Opal::Erubi::Processor, *extra_args
 end
-
-Sprockets.register_engine '.erubi', Opal::Erubi::Processor, *extra_args
